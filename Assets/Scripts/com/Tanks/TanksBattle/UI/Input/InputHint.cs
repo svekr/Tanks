5a824380d07fc6cd379389fe499df17ee920aca3
Andrey Buryak <sviokr@gmail.com>
2024-07-14 22:07:21 +0300

    Add input hints.

---
+++ Assets/Scripts/com/Tanks/TanksBattle/UI/Input/InputHint.cs
@@ -0,0 +1,24 @@
+﻿using com.Tanks.TanksBattle.Tank.Movement;
+using UnityEngine;
+
+namespace com.Tanks.TanksBattle.UI.Input {
+    public class InputHint : MonoBehaviour {
+        [SerializeField] private TankMovementType _movementType;
+        [SerializeField] private MovementTypeSwitcher _switcher;
+
+        private void Awake() {
+            if (_switcher == null) return;
+            gameObject.SetActive(_movementType == _switcher.CurrentType);
+            _switcher.OnSwitch += OnSwitch;
+        }
+
+        private void OnDestroy() {
+            if (_switcher == null) return;
+            _switcher.OnSwitch -= OnSwitch;
+        }
+
+        private void OnSwitch(TankMovementType movementType) {
+            gameObject.SetActive(_movementType == movementType);
+        }
+    }
+}
